c7d45baba7cdddab321ed681a2cd35df91544085
Jose Renteria <JoseRenteria.sf@outlook.com>
2018-04-02 08:01:52 -0700

    resctructured for deployment

---
+++ server.js
@@ -0,0 +1,15 @@
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './app/assets/', 'index.html'));
+});
+
+app.listen(port);
+console.log("App listening on port " + port);
